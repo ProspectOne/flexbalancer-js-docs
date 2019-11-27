e2f1db8ca41fd4ac194ced59a950307ad24119d2
HesyRa <bosmerer@gmail.com>
2019-11-27 15:31:08 +0100

    POD-701 #comment Optimal Round Trip Time with Sonar Availability snippet. Updated

--- examples/optional-round-trip-time-with-sonar-availability.ts
+++ examples/optional-round-trip-time-with-sonar-availability.ts
@@ -1,208 +1,72 @@
-// weighted-round-robin
-interface Settings {
-    providers:{[key: string]:{cname: string, monitorId: number}},
-    default_ttl:number,
-    availability_threshold: number
-}
-const settings:Settings = {
-    // The array of all possible responses. The key, e.g. 'provider1', is the
-    // label for the platform. The value, e.g. 'cname1.foo.com' is the CNAME
-    // to hand back when that platform is selected.
-
-    // Round Robin weight (these are relative to one another for purposes of
-    // weighted random selection, but it may be useful to think of them as
-    // percentages (i.e. they add up to 100).
-    providers: {
-        'https://www.jsdelivr.com/': {
-            monitorId: 301,
+// Optimal Round Trip Time with Sonar Availability
+const configuration = {
+    providers: [
+        {
+            name: ('jsdelivr-cdn' as TCDNProvider),
+            monitorId: (301 as TMonitor),
             cname: 'www.foo.com'
         },
-        'https://www.cloudflare.com': {
-            monitorId: 302,
+        {
+            name: ('cloudflare' as TCDNProvider),
+            monitorId: (302 as TMonitor),
             cname: 'www.bar.com'
         },
-        'https://cloud.google.com/cdn/': {
-            monitorId: 303,
+        {
+            name: ('google-cloud-cdn' as TCDNProvider),
+            monitorId: (303 as TMonitor),
             cname: 'www.baz.com'
         }
-    },
+    ],
 
     // The DNS TTL to be applied to DNS responses in seconds.
-    default_ttl: 20,
-    availability_threshold: 90
-}
-
-const allReasons = {
-    best_performing_provider: 'A',
-    data_problem: 'B',
-    all_providers_eliminated: 'C'
+    defaultTtl: 20,
+    availabilityThreshold: 90
 };
 
-const dataAvail = getProbe('avail')
-const dataSonar = parseSonarData(getData('sonar'));
-
-var aliases = settings.providers === undefined ? [] : Object.keys(settings.providers);
-
-function getProbe(property:string) {
-    let result = {};
-    const data = Object.keys(settings.providers);
-    if(property === 'avail') {
-        data.forEach(value => {
-            if(!result[value]) result[value] = {};
-            result[value].property = fetchCdnRumUptime(value as CDNProvider);
-        });
-    }
-    if(property === 'http_rtt') {
-        data.forEach(value => {
-            if(!result[value]) result[value] = {};
-            result[value].property = fetchCdnRumPerformance(value as CDNProvider);
-        });
-    }
-    return result;
-}
-/**
- * @param {!Object} object
- * @param {Function} filter
- */
-function filterObject(object, filter) {
-    var keys = Object.keys(object),
-        i = keys.length,
-        key,
-        candidates = {};
-
-    while (i --) {
-        key = keys[i];
-
-        if (filter(object[key], key)) {
-            candidates[key] = object[key];
-        }
-    }
-
-    return candidates;
-}
-
-/**
- * @param {!Object} source
- * @param {string} property
- */
-function getHighest(source, property) {
-    var keys = Object.keys(source),
-        i = keys.length,
-        key,
-        candidate,
-        max = -Infinity,
-        value;
-    while (i --) {
-        key = keys[i];
-        value = source[key][property];
-        if (value > max) {
-            candidate = key;
-            max = value;
-        }
-    }
-    return candidate;
-}
-
-/**
- * @param candidate
- * @param key
- * @returns {boolean}
- */
-function filterAvailability(candidate, key) {
-    return dataAvail[key] !== undefined && dataAvail[key].avail >= settings.availability_threshold;
-}
-
-/**
- * @param {!Object} data
- */
-function parseSonarData(data) {
-    var keys = Object.keys(data),
-        i = keys.length,
-        key;
-    while (i --) {
-        key = keys[i];
-        try {
-            data[key] = JSON.parse(data[key]);
-        }
-        catch (e) {
-            delete data[key];
-        }
-    }
-    return data;
-}
-
-/**
- * @param {!Object} source
- * @param {string} property
- */
-function getLowest(source, property) {
-    var keys = Object.keys(source),
-        i = keys.length,
-        key,
-        candidate,
-        min = Infinity,
-        value;
-    while (i --) {
-        key = keys[i];
-        value = source[key][property];
-        if (value < min) {
-            candidate = key;
-            min = value;
-        }
-    }
-    return candidate;
-}
-
-/**
- * @param candidate
- * @param key
- */
-function filterSonar(candidate, key) {
-    return dataSonar[key] !== undefined && dataSonar[key].avail > 0;
-}
-
-function getData(property) {
-    let data = {};
-    const providers = Object.keys(settings.providers);
-    if (property === 'sonar') {
-        providers.forEach(value => {
-            if(!data[value]) data[value] = {};
-            data[value] = JSON.stringify({
-                'avail': isMonitorOnline(settings.providers[value].monitorId as Monitor) ? 1: 0
-            })
-        })
-    }
-    return data;
-}
+const getHighest = (array: number[]): number => array.indexOf(Math.max(...array));
+const getHighestByProperty = <T>(array: T[], property):T => array[getHighest(array.map(item => item[property]))];
+const getLowest = (array: number[]): number => array.indexOf(Math.min(...array));
+const getLowestByProperty = <T>(array: T[], property):T => array[getLowest(array.map(item => item[property]))];
 
 async function onRequest(req: Request, res: Response) {
-    let candidates, decisionProvider, decisionReason, totalWeight
-    candidates = getProbe('http_rtt')
-    //@TODO dataAvail -> uptime data
-    if (Object.keys(candidates).length > 0) {
-        // Select the best performing provider that meets its minimum
-        // availability score, if given
-        if (Object.keys(dataSonar).length > 0) {
-            // remove any sonar unavailable
-            candidates = filterObject(candidates, filterSonar);
-        }
-        if (Object.keys(candidates).length > 0 && Object.keys(dataAvail).length > 0) {
-            candidates = filterObject(candidates, filterAvailability);
-            if (Object.keys(candidates).length > 0) {
-                decisionProvider = getLowest(candidates,'http_rtt');
-                decisionReason = allReasons.best_performing_provider;
-            } else {
-                decisionProvider = getHighest(dataAvail, 'avail');
-                decisionReason = allReasons.all_providers_eliminated;
-            }
+    const { providers, defaultTtl, availabilityThreshold } = configuration;
+
+    const monitorFilteredProviders = providers.filter(
+        (provider) => isMonitorOnline(provider.monitorId)
+    );
+    // If all providers are down return random
+    if (monitorFilteredProviders.length === 0) {
+        return {
+            addr: providers[Math.floor(Math.random() * providers.length)].cname,
+            ttl: defaultTtl
         }
     }
-    if (decisionProvider === undefined) {
-        decisionProvider = aliases[Math.floor(Math.random() * aliases.length)];
-        decisionReason = allReasons.data_problem;
+    const availableFilteredProviders = monitorFilteredProviders.filter(
+        (provider) => fetchCdnRumUptime(provider.name) > availabilityThreshold
+    );
+    // If available providers return with lowest performance
+    if (availableFilteredProviders.length) {
+        const perfProvidersData = availableFilteredProviders.map(
+            (provider) => ({
+                provider,
+                perf: fetchCdnRumPerformance(provider.name)
+            })
+        );
+        return {
+            addr: getLowestByProperty(perfProvidersData, 'perf').provider.cname,
+            ttl: defaultTtl
+        }
     }
-    // response.setReasonCode(decisionReason);
-    res.addr = [settings.providers[decisionProvider].cname];
-    res.ttl = settings.default_ttl
-    return res
+
+    // Fallback. Take highest performance from All providers
+    const perfProvidersData = providers.map(
+        (provider) => ({
+            provider,
+            perf: fetchCdnRumPerformance(provider.name)
+        })
+    );
+    return {
+        addr: getHighestByProperty(perfProvidersData, 'perf').provider.cname,
+        ttl: defaultTtl
+    }
 }
