8646d62cb7ef137eb148eff6ef6318c1bea9866c
HesyRa <bosmerer@gmail.com>
2019-11-28 16:49:33 +0100

    POD-699 #comment Updated according to our style and added more comments

--- examples/multi-geo-round-robin-with-overrides-monitor-snippet.ts
+++ examples/multi-geo-round-robin-with-overrides-monitor-snippet.ts
@@ -1,116 +1,86 @@
-//Definition for our answer type
-type TAnswer =
-    {
-        name: string,
-        cname: string,
-        monitor?: TMonitor
-    };
-
-//List of answers
-const answers: TAnswer[] = [
-    {
-        name: 'first',
-        cname: 'www.first.com',
-        monitor: 304
-    },
-    {
-        name: 'second',
-        cname: 'www.second.com',
-        monitor: 305
-    },
-    {
-        name: 'third',
-        cname: 'www.third.com'
-    },
-    {
-        name: 'fourth',
-        cname: 'www.fourth.com'
-    }
-];
-
-//Default answer that will be returned
-//if no acceptable candidate will be found
-const defaultAnswer: string = "fourth";
-
-//Rules that will define answer accordint to country
-//from which request was made
-const countriesAnswersRoundRobin = {
-    'PL': ['first','second'],
-    'JP': ['third']
-};
-
-//Function return random object element
-const getRandomElement = (object) => {
-    return object[Math.floor(Math.random() * Object.keys(object).length)];
-};
-
-//Add answer as candidate if it doesnt have monitor
-const requireMonitorData: boolean = false;
-
-//Filter function to check is provided answer is acceptable by checking its monitor
-//or if there are no monitor and requireMonitorData is set to true then it will count as acceptable
-const isProperCandidate = (candidate: TAnswer) => {
-    if (candidate.monitor) {
-        if (isMonitorOnline(candidate.monitor)) {
-            return true;
+const configuration = {
+    providers: [
+        {
+            name: 'foo', // candidate name
+            cname: 'www.foo.com', // cname to pick as a result
+            monitor: (304 as TMonitor) // Monitor ID which is created by user to monitor hostname
+        },
+        {
+            name: 'bar',
+            cname: 'www.bar.com',
+            monitor: (305 as TMonitor)
+        },
+        {
+            name: 'third',
+            cname: 'www.baz.com'
+        },
+        {
+            name: 'origin',
+            cname: 'www.origin.com'
         }
-    } else if (requireMonitorData) {
-        return true;
-    }
-    return  false;
+    ],
+    countriesAnswersRoundRobin: {
+        'PL': ['bar', 'baz'],
+        'JP': ['foo']
+    },
+    defaultTtl: 20,
+    requireMonitorData: false
 };
 
-async function onRequest(request: IRequest, response: IResponse) {
-    //End list of candidates of answers
-    let candidates: object[] = [];
-    //Country where request was made from
-    let requestCountry = request.location.country;
+/**
+ * Function return random object element from array
+ * @param items
+ */
+const getRandomElement = <T>(items: T[]): T => {
+    return items[Math.floor(Math.random() * items.length)];
+};
 
-    //Checking did we managed to detect country,
-    //does our country listed in countriesAnswersRoundRobin list
-    //and does it contains any answers
-    if (requestCountry &&
-        countriesAnswersRoundRobin[requestCountry] &&
-        countriesAnswersRoundRobin[requestCountry].length) {
-
-        countriesAnswersRoundRobin[requestCountry].forEach( (answerName) => {
-            //Collect all the answers
-            let geoCandidates = answers.filter((answerCheck) =>  {
-                return (answerCheck.name === answerName);
-            });
-
-            if (geoCandidates.length) {
-                //Filter geo answers by defined earlier monitors check
-                geoCandidates.filter(isProperCandidate).forEach( (filtered) => {
-                    //Save answers for later use
-                    candidates.push(filtered);
-                });
-            }
-        });
+/**
+ * If monitor is set for candidate returns its availability, else returns true if monitor is not required
+ * @param candidate
+ * @param requireMonitorData
+ */
+const isProperCandidate = (candidate, requireMonitorData) => {
+    if (candidate.monitor) {
+        return isMonitorOnline(candidate.monitor)
     }
+    return !requireMonitorData;
+};
 
-    //If we found proper geo candidates, return one of them by random
-    if (candidates.length) {
-        response.addr = getRandomElement(candidates).cname;
-        return response;
+async function onRequest(req: IRequest, res: IResponse) {
+    const {countriesAnswersRoundRobin, providers, defaultTtl, requireMonitorData} = configuration;
+
+    // Country where request was made from
+    let requestCountry = req.location.country;
+
+    // Checking did we managed to detect country, does our country listed in countriesAnswersRoundRobin list
+    if (requestCountry && countriesAnswersRoundRobin[requestCountry]) {
+        // Choose candidates that are listed in countriesAnswersRoundRobin and are proper candidates
+        let geoFilteredCandidates = providers.filter(
+            (provider) => provider.name in countriesAnswersRoundRobin[requestCountry] && isProperCandidate(provider, requireMonitorData)
+        );
+        // If we found proper geo candidates, return one of them by random
+        if (geoFilteredCandidates) {
+            return {
+                addr: getRandomElement(geoFilteredCandidates).cname,
+                ttl: defaultTtl
+            }
+        }
     }
 
     //If there was no geo candidates, we choose new ones from whole list by monitor filter
-    candidates = answers.filter(isProperCandidate);
+    const properCandidates = providers.filter(item => isProperCandidate(item, requireMonitorData));
 
     //Choose random candidate as response if we have any
-    if (candidates.length) {
-        response.addr = getRandomElement(candidates).cname;
-        return response;
+    if (properCandidates) {
+        return {
+            addr: getRandomElement(properCandidates).cname,
+            ttl: defaultTtl
+        }
     }
-
-    let setDefaultAnswer = answers.find(answer => answer.name === defaultAnswer);
-
-    //Set default as response
-    if (setDefaultAnswer) {
-        response.addr = setDefaultAnswer.cname;
-        return response;
-    }
-
-    return response;
+    // Fallback pick 'origin' cname
+    return {
+        addr: providers['origin'].cname,
+        ttl: defaultTtl
+    };
 }
