a8a26f3a1adb011b3ad0bba1df83df850a010439
HesyRa <bosmerer@gmail.com>
2019-11-28 16:14:09 +0100

    POD-698 #comment Added more comments

--- examples/weighted-round-robin-snippet.ts
+++ examples/weighted-round-robin-snippet.ts
@@ -1,10 +1,12 @@
-// weighted-round-robin
+// Weighted Round Robin
+// Main configuration
 const configuration = {
+    /** List of  providers configuration */
     providers: [
         {
-            name: ('jsdelivr-cdn' as TCDNProvider),
-            cname: 'cname1.foo.com',
-            weight: 50
+            name: ('jsdelivr-cdn' as TCDNProvider),// CDN Provider alias to work with
+            cname: 'cname1.foo.com',// cname to pick as a result
+            weight: 50// Weight number for this choice
         },
         {
             name: ('google-cloud-cdn' as TCDNProvider),
@@ -17,28 +19,41 @@ const configuration = {
             weight: 20
         }
     ],
-
-    // The DNS TTL to be applied to DNS responses in seconds.
-    defaultTtl: 20,
-    availabilityThreshold: 90
+    defaultTtl: 20, // The DNS TTL to be applied to DNS responses in seconds.
+    availabilityThreshold: 90 // Board value for providers 'Uptime' to compare with
 };
 
+/**
+ * returns index of Highest value in array
+ * @param array
+ */
 const getHighest = (array: number[]): number => array.indexOf(Math.max(...array));
+/**
+ * returns object which have highest value of property
+ * @param array
+ * @param property
+ */
 const getHighestByProperty = <T>(array: T[], property):T => array[getHighest(array.map(item => item[property]))];
-const getSumByProperty = <T>(items:T[], property): number => items.reduce((sum, item) => sum += item[property],0)
-
+/**
+ * return Sum of property value for each object in array
+ * @param items
+ * @param property
+ */
+const getSumByProperty = <T>(items:T[], property): number => items.reduce((sum, item) => sum += item[property],0);
 
 async function onRequest(req: IRequest, res: IResponse) {
     const {providers, defaultTtl, availabilityThreshold} = configuration;
 
+    // Choose providers that have 'UPTIME' value more that threshold.
     const availableProviders = providers.filter(
-        // uptime data for 10 minutes
         (provider) => fetchCdnRumUptime(provider.name) > availabilityThreshold
     );
+    // Calculate total weight for all providers
     const totalWeight = getSumByProperty(availableProviders, 'weight');
 
-    //Fallback
+    // If providers list result after filter is empty or total weight less or equal to 0 go with fallback option
     if (availableProviders.length === 0 || totalWeight <= 0) {
+        // Create array map with 'uptime' value for each provider
         const CDNUptimeData = providers.map(
             // uptime data for 10 minutes
             (provider) => ({
@@ -46,18 +61,20 @@ async function onRequest(req: IRequest, res: IResponse) {
                 uptime: fetchCdnRumUptime(provider.name)
             })
         );
+        // Return Object with default ttl and take providers from array with highest uptime' value
         return {
             addr: getHighestByProperty(CDNUptimeData, 'uptime').provider.cname,
             ttl: defaultTtl
         };
     }
+    // If we have single available result simply return it
     if (availableProviders.length === 1) {
         return {
             addr: availableProviders[0].cname,
             ttl: defaultTtl
         };
     }
-
+    // If we have bunch of available result pick one randomly weighted
     const random = Math.floor(Math.random() * totalWeight);
     let mark = 0;
     let weightedProviderIndex = 0;
@@ -68,7 +85,7 @@ async function onRequest(req: IRequest, res: IResponse) {
             break;
         }
     }
-
+    // Return Object with default ttl and chosen provider
     return {
         addr: availableProviders[weightedProviderIndex].cname,
         ttl: defaultTtl
