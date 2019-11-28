9b928bf3df3eac53af258dc171a3c70436964304
Artem Stoianov <artem.s@perfops.net>
2019-11-28 16:36:07 +0100

    POD-702: Measure performance and availability on a country level

---
+++ examples/sticky-perf-with-penalty-and-availability.ts
@@ -0,0 +1,76 @@
+// Basic configuration object for our FlexBalancer application
+const configuration = {
+    // List of providers we are interested in
+    // `name` must be one of the valid provider aliases from TCDNProvider type
+    providers: [
+        {
+            name: 'jsdelivr-cdn' as TCDNProvider,
+            cname: 'www.foo.com',
+            padding: 0,
+            ttl: 20, // We can provide different TTL for different providers
+        },
+        {
+            name: 'akamai' as TCDNProvider,
+            cname: 'www.bar.com',
+            padding: 0,
+        },
+        {
+            name: 'cloudflare' as TCDNProvider,
+            cname: 'www.baz.com',
+            padding: 5,
+        }
+    ],
+
+    // Default provider which will be chosen if no suitable providers will be found.
+    default_provider: 'jsdelivr-cdn',
+
+    // The TTL to be set when the application chooses a provider.
+    default_ttl: 30,
+
+    // Minimum threshold under which provider will be considered unavailable
+    availability_threshold: 97,
+};
+
+async function onRequest(req: IRequest, res: IResponse) {
+    // Get availability for each provider
+    const providersAvailability = configuration.providers.map((provider) => {
+        // If we know from which country request is - get availability for it
+        // If no - get a global scores
+        const availability = req.location.country ?
+            fetchCdnRumUptime(provider.name, 'country', req.location.country) :
+            fetchCdnRumUptime(provider.name);
+        return { provider, availability }
+    });
+
+    // Filter by availability threshold
+    const availableProviders = providersAvailability.filter(data => data.availability > configuration.availability_threshold);
+
+    // Get performance and apply paddings for available providers
+    const providersPerformance = availableProviders.map(data => ({
+        provider: data.provider,
+        performance: req.location.country ?
+            // Get performance for country if we know it
+            fetchCdnRumPerformance(data.provider.name, 'country', req.location.country) + data.provider.padding :
+            // Get global performance instead
+            fetchCdnRumPerformance(data.provider.name) + data.provider.padding
+    }));
+
+    // Start decision process
+    let decision: any = null;
+
+    // If we have a providers to choose from - choose one with the best performance
+    if (providersPerformance.length) {
+        decision = providersPerformance.sort((a, b) => a.performance - b.performance)[0].provider;
+    }
+
+    // No available providers - return default
+    if (!decision) {
+        decision = configuration.providers.find(provider => provider.name === configuration.default_provider)
+    }
+
+    // Prepare response
+    res.addr = decision.cname;
+    res.ttl = decision.ttl ? decision.ttl : configuration.default_ttl;
+
+    return res;
+}
